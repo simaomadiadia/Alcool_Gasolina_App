debaa4006b12e2f4ab7f55fd699d8fbbf207b23c
unknown <simaomadiadia@gmail.com>
2026-01-11 16:37:02 +0000

    adcionar 2 caixas de texto, uma imagem de logo, um titulo e um botão

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,60 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.blue,
         title: Text("Álcool ou gasolina"),
       ),
+      body: Center(
+        child: Container(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: Image.asset("Imagens/logo.png"),
+              ),
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: Text(
+                  "Saiba qual a melhor opção para abastecimento do seu carro.",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: "Preço Alcool ex:1.59"
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: "Preço Gasolina ex:3.59"
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: (){
+                    
+                  }, 
+                  child: Text("Calcular"),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.white,
+                  fixedSize:  Size(350,45),
+
+
+                ),
+              )
+              
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
